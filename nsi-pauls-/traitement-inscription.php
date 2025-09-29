e42f9e76e2299cdb1d1290cc86e310f246852844
Luxio25 <glitchmontage.lux@gmail.com>
2025-09-29 21:16:12 +0200

    Vérification mail existe

--- nsi-pauls-/traitement-inscription.php
+++ nsi-pauls-/traitement-inscription.php
@@ -2,73 +2,59 @@
 
 <?php 
 
-try
-{
-       $bdd = new PDO('mysql:host=localhost;dbname=nsilps', 'root', 'root');
-       $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=nsilps', 'root', 'root');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
 }
-catch (Exception $e)
-{
-       die('Erreur : ' . $e->getMessage());
-}
-
 
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 $ddn = $_POST['ddn'];
-$mail = $_POST['mail'];
+$mailtext = $_POST['mail'];
 $mdp = $_POST['mdp'];
 $verifmdp = $_POST['verifmdp'];
 
-try
-{
+$verifyemail = array();
+
+try {
        $inscription = $bdd->prepare('INSERT INTO utilisateur(Nom, Prenom, mail, mdp, ddn, idclasse, type) VALUES(:Nom, :Prenom, :mail, :mdp, :ddn, :idclasse, :type)');
-}
-catch (Exception $e)
-{
+} catch (Exception $e) {
        die('Erreur : ' . $e->getMessage());
 }
 
-try
-{
-       $verify_email = $bdd->prepare('SELECT mail FROM utilisateur WHERE mail=:mail');
+try {
+       $verify_email = $bdd->prepare('SELECT mail FROM utilisateur WHERE mail = :mail');
+       $verify_email->execute([
+              'mail' => $mailtext
+       ]);
+       $verifyemail = $verify_email->fetch();
+
+       if ($verifyemail) {            
+              echo "L'adresse mail existe déjà !";
+              header('Location: erreur/101.html');
+              exit();
+       } else {
+              if ($mdp == $verifmdp) {
+                     $inscription->execute([
+                            'Nom'      => $nom,
+                            'Prenom'   => $prenom,
+                            'mail'     => $mailtext,
+                            'mdp'      => $mdp,
+                            'ddn'      => $ddn,
+                            'idclasse' => 0,
+                            'type'     => 'e'
+                     ]);
+
+                     echo 'Inscription réussie !';
+                     header('Location: ../SiteNSISamuel/index.html');
+                     exit();
+              } else {
+                     echo 'Les mdp ne correspondent pas';
+              }
+       }
+} catch (Exception $e) {
+    die('Erreur SQL : ' . $e->getMessage());
 }
-catch (Exception $e)
-{
-       die('Erreur : ' . $e->getMessage());
-}
-
-$verify_email->execute(array(
-       'mail'=> $mail
-));
-
-sleep(1);
-
-$verifyemail = $verify_email->fetch()
-    
-if ($verify_email==$mail) {            
-       echo "L'adresse mail existe déjà !";
-       header('Location: erreur/101.html');
-       exit();
-};
-if ($mdp==$verifmdp) {
-       $inscription->execute(array(
-              'Nom'=> $nom,
-              'Prenom'=> $prenom,
-              'mail'=> $mail,
-              'mdp'=> $mdp,
-              'ddn'=> $ddn,
-              'idclasse' => '0',
-              'type' => 'e'
-       ));
-
-       echo'Inscription réussie !';
-       #header('Location: ../SiteNSISamuel/index.html');
-       #exit();
-}
-else
-       echo'les mdp ne correspondent pas';
-
-$inscription->closeCursor();
 ?>
