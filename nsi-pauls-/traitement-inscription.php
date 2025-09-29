83b07baba82578a0ef5d23ac98a7aaf69d9edb81
Luxio25 <glitchmontage.lux@gmail.com>
2025-09-29 17:58:45 +0200

    b

--- nsi-pauls-/traitement-inscription.php
+++ nsi-pauls-/traitement-inscription.php
@@ -11,6 +11,43 @@ catch (Exception $e)
        die('Erreur : ' . $e->getMessage());
 }
 
-$bdd->exec('INSERT INTO Utilisateur(Nom, Prenom, ddn, idclasse, type) VALUES(:nom, :Prenom, :)');
 
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$ddn = $_POST['ddn'];
+$mail = $_POST['mail'];
+$mdp = $_POST['mdp'];
+$verifmdp = $_POST['verifmdp'];
+
+try
+{
+       $inscription = $bdd->prepare('INSERT INTO utilisateur VALUES(:id, :Nom, :Prenom, :ddn, :idclasse, :type)');
+}
+catch (Exception $e)
+{
+       die('Erreur : ' . $e->getMessage());
+}
+
+try
+{
+       if ($mdp==$verifmdp) {
+       $inscription->execute(array(
+              'id' => null,
+              'Nom'=> $nom,
+              'Prenom'=> $prenom,
+              'ddn'=> $ddn,
+              'idclasse' => 'sans_classe',
+              'type' => 'e'
+       ));
+       echo'Les mdp correspondent';
+       }
+       else
+              echo'les mdp ne correspondent pas';
+}
+catch (Exception $e)
+{
+       die('Erreur : ' . $e->getMessage());
+}
+
+$inscription->closeCursor();
 ?>
