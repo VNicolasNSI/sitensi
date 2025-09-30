4bb1ac02a15624842418a1a3d07806951bfd1def
Zeta <samuelanjot.snt@gmail.com>
2025-09-30 09:42:47 +0200

    php

--- SiteNSISamuel/code.php
+++ SiteNSISamuel/code.php
@@ -1,5 +1,16 @@
 <html>
 
+<?php 
+
+try {
+    $bdd = new PDO('mysql:host=sql7.freesqldatabase.com;dbname=sql7800701', 'sql7800701', 'bfhPTiR56K');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+?>
+
 <head>
 <title>GoofyAhhSite</title> 
 
