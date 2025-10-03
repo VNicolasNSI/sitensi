1dbd1a43823807f0624439947cbb1378b82d0599
VNICOLAS <valentinnicolas.snt@gmail.com>
2025-10-03 14:01:22 +0200

    page delete

---
+++ nsi-pauls-/supp.php
@@ -0,0 +1,12 @@
+<?php 
+
+try {
+    $bdd = new PDO('mysql:host=sql7.freesqldatabase.com;dbname=sql7800701', 'sql7800701', 'bfhPTiR56K');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+$supp=$bdd->query('DELETE FROM utilisateur')
+
+?>
