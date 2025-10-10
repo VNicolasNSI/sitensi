c1eb825496c07aa72f42aa0db61c78bd7164eafc
Zeta <samuelanjot.snt@gmail.com>
2025-10-10 09:31:13 +0200

    d

--- Premiere.php
+++ Premiere.php
@@ -49,7 +49,7 @@ try {
 		</article>
 	</section>
 	<section class="partie2">
-		<h1>Parcours de première</h1><br>
+		<article><h1>Parcours de première</h1><br></article>
 		<article class="connexion">
 			<a role="button" href="depot.php" class="attributa">
 			<img width="50" height="50" title="1" src="https://cdn-icons-png.flaticon.com/512/80/80942.png">
