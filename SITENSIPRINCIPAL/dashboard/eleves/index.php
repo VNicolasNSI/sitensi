c10ec7f2a666a82d22d43b4891d4d4393562e148
Zeta <samuelanjot.snt@gmail.com>
2025-10-14 09:07:42 +0200

    encore test

--- SITENSIPRINCIPAL/dashboard/eleves/index.php
+++ SITENSIPRINCIPAL/dashboard/eleves/index.php
@@ -56,17 +56,17 @@ try {
 	</article>
 </section>
 <section class="dispo1">
+	<article>
+		<img width="50" height="50" title="1" src="https://cdn-icons-png.flaticon.com/512/80/80942.png">
+	</article>
 	<article class="connexion">
 		<a role="button" href="depot.php" class="attributa">
 		Cours PDF
 		</a>
 	</article>
-	<article>
-		<img width="50" height="50" title="1" src="https://cdn-icons-png.flaticon.com/512/80/80942.png">
-	</article>
 </section>
 <section class="dispo2">
-	<article class="connexion">
+	<article class="grossissement">
 		<a role="button" href="synthese.php" class="attributa">
 		<img width="50" height="50" title="8" src="https://cdn-icons-png.flaticon.com/512/722/722013.png">
 		Synthèse / Progression
