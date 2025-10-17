ceff1a4d4d9ba0c9acb052f031bef9541658e5b5
Zeta <samuelanjot.snt@gmail.com>
2025-10-17 08:55:42 +0200

    code

--- SITENSIPRINCIPAL/dashboard/eleves/code.php
+++ SITENSIPRINCIPAL/dashboard/eleves/code.php
@@ -23,7 +23,7 @@ try {
 <body class="body2">
 <section>
     <form method=post>
-	    <label for="nom">Entrez un code : </label> 
+	    <label for="nom">Entrez un code : </label>
 	    <input id="nom" type="text" name="nom" placeholder="--Ici---" size=10 maxlength=4 minlenght=4>
 	    <input type=submit value="Envoyer">
     </form>
