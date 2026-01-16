48d8fdcbd9d9f296c7b83047d64f4bc3e2490f8d
VNICOLAS <valentinnicolas.snt@gmail.com>
2026-01-16 15:03:19 +0100

    fix sql connect

--- SITENSIPRINCIPAL/Accueil/traitement-connexion.php
+++ SITENSIPRINCIPAL/Accueil/traitement-connexion.php
@@ -5,7 +5,7 @@
 session_start();
 
 try {
-    $bdd = new PDO('mysql:host=149.56.45.233:3306;dbname=s8517_mirai', 'u8517_ne3b7zkKmC', 'Lq8Vx@A+BMOh5z50orUqY@rv');
+    $bdd = new PDO('mysql:host=localhost;dbname=nsilps', 'root', 'root');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
